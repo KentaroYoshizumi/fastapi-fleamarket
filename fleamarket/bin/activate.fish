b0ced5652d17f0c37262bba3a5d5bb261400cd89
KentaroYoshizumi <schecter3.2.1-@softbank.ne.jp>
2024-11-15 00:15:58 +0900

    first commit

--- fleamarket/bin/activate.fish
+++ fleamarket/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kentaroyoshizumi/Desktop/FastAPI/fleamarket"
+set -gx VIRTUAL_ENV "/Users/kentaroyoshizumi/Desktop/fastapi-fleamarket-main/fleamarket"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from cruds import item as item_cruds
 
 app = FastAPI()
 
-@app.get("/")
-async def example():
-    return {"message": "Hello World!"}
+@app.get("/items")
+async def find_all():
+  return item_cruds.find_all()
