5c19103a0c36894d9e52d979a229353226613a7e
KentaroYoshizumi <schecter3.2.1-@softbank.ne.jp>
2024-11-19 22:18:03 +0900

    create function

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from cruds import item as item_cruds
 
 app = FastAPI()
 
 @app.get("/items")
 async def find_all():
-  return item_cruds.find_all()
+    return item_cruds.find_all()
 
 @app.get("/items/{id}")
 async def find_by_id(id: int):
-  return item_cruds.find_by_id(id)
+    return item_cruds.find_by_id(id)
 
 @app.get("/items/")
 async def find_by_name(name: str):
-  return item_cruds.find_by_name(name)
+    return item_cruds.find_by_name(name)
+
+@app.post("/items")
+async def create(item_create=Body()):
+    return item_cruds.create(item_create)
